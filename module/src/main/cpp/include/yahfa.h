77d4af74b6e5d487b56e1b192f3ac057c9b8d176
Gstalker <dreamstalker1003@qq.com>
2022-04-13 19:32:24 +0800

    feat: import yahfa; refector: move rust to src/main/rust

---
+++ module/src/main/cpp/include/yahfa.h
@@ -0,0 +1,13 @@
+//
+// Created by Dreamstalker on 2021/9/7.
+//
+
+#ifndef UNPACKER_AADEBUGMODULE_YAHFA_H
+#define UNPACKER_AADEBUGMODULE_YAHFA_H
+
+#include "jni.h"
+
+extern JNINativeMethod yahfa_hookmain_methods[];
+extern JNINativeMethod yahfa_utils_methods[];
+
+#endif //UNPACKER_AADEBUGMODULE_YAHFA_H
