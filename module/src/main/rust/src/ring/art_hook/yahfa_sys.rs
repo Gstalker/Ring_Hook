22984b7aa8a53d9864897e6d6e6f792793ddcaba
Gstalker <dreamstalker1003@qq.com>
2022-04-18 22:11:14 +0800

    fix: type error of yahfa_sys.rs

--- module/src/main/rust/src/ring/art_hook/yahfa_sys.rs
+++ module/src/main/rust/src/ring/art_hook/yahfa_sys.rs
@@ -1,6 +1,7 @@
 use jni;
 use std::sync::Mutex;
 use std::ffi::CString;
+use std::ffi::CStr;
 
 #[link(name = "yahfa")]
 extern "C"{
@@ -10,18 +11,18 @@ extern "C"{
 
 pub fn get_yahfa_hook_main_methods_vec() -> (String,Vec<jni::NativeMethod>) {
     let yahfa_hook_main_methods_ptr = unsafe{ get_yahfa_hook_main_methods() };
-    let yahfa_hook_main_methods_sys :Vec<jni::sys::JNINativeMethod> = unsafe{
+    let yahfa_hook_main_methods_sys = unsafe{
         // length and capacity from module/src/main/cpp/yahfa/include/yahfa.h
-        Vec::from_raw_parts(yahfa_hook_main_methods_ptr,3 ,3)
+        std::ptr::slice_from_raw_parts(yahfa_hook_main_methods_ptr,3).as_ref().unwrap()
     };
     let yahfa_hook_main_methods = yahfa_hook_main_methods_sys
         .iter()
         .map(|nm| jni::NativeMethod {
             name: jni::strings::JNIString::from(
-                unsafe{ CString::from_raw(nm.name).into_string().unwrap() }
+                unsafe{ CStr::from_ptr(nm.name).to_str().unwrap().to_string() }
             ),
             sig: jni::strings::JNIString::from(
-                unsafe{ CString::from_raw(nm.signature).into_string().unwrap() }
+                unsafe{ CStr::from_ptr(nm.signature).to_str().unwrap().to_string() }
             ),
             fn_ptr: nm.fnPtr
         })
@@ -33,16 +34,16 @@ pub fn get_yahfa_utils_native_methods_vec() -> (String, Vec<jni::NativeMethod>) 
     let yahfa_utils_methods_ptr = unsafe{ get_yahfa_utils_methods() };
     let yahfa_utils_methods_sys = unsafe{
         // length and capacity from module/src/main/cpp/yahfa/include/yahfa.h
-        Vec::from_raw_parts(yahfa_utils_methods_ptr, 3, 3)
+        std::ptr::slice_from_raw_parts(yahfa_utils_methods_ptr,3).as_ref().unwrap()
     };
     let yahfa_utils_native_methods = yahfa_utils_methods_sys
         .iter()
         .map(|nm| jni::NativeMethod {
             name: jni::strings::JNIString::from(
-                unsafe{ CString::from_raw(nm.name).into_string().unwrap() }
+                unsafe{ CStr::from_ptr(nm.name).to_str().unwrap().to_string() }
             ),
             sig: jni::strings::JNIString::from(
-                unsafe{ CString::from_raw(nm.signature).into_string().unwrap() }
+                unsafe{ CStr::from_ptr(nm.signature).to_str().unwrap().to_string() }
             ),
             fn_ptr: nm.fnPtr,
         })
